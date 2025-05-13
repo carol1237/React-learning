4bf5ecaa621519565b176c04cdd51e6cd3d16c86
carol1237 <carolngkf@hotmail.com>
2025-05-14 03:38:32 +1000

    Delete src directory

--- src/Title.js
+++
@@ -1,9 +0,0 @@
-export default function Title({mainTitle}) {
-    return (
-        <div>
-            <h1 style={{borderBottom : '5px solid red', textAlign:"center"}}>
-                {mainTitle}
-            </h1>
-        </div>
-    )
-}
